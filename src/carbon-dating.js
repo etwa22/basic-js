fdc56c6d58b5b29b1d2a0354fa771ab861611c61
etwa22 <onadeina1@mail.ru>
2023-12-23 07:52:43 +0300

    feat: update

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity === 'string' && Number(sampleActivity) > 0 && Number(sampleActivity) < 15) {
+    return - Math.floor(Math.log(15 / Number(sampleActivity)) * 5730 / (Math.log(0.5)));
+  }
+  return false;
 }
 
 module.exports = {
