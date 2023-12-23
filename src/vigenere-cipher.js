7c69943ec8125c1bbbfa25e7c062574462dfa9b0
etwa22 <onadeina1@mail.ru>
2023-12-23 07:12:21 +0300

    feat: update

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,71 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let index = 0;
+    let messageArray = message.toUpperCase().split('');
+    let keyArray = key.toUpperCase().split('');
+    
+    let result = messageArray.map((char) => {
+      if (this.alphabet.includes(char)){
+        if (index > keyArray.length - 1) {index = 0}
+        console.log(index, char, keyArray[index]);
+        let keyChar = keyArray[index];
+        index += 1;
+        let encrypted = this.alphabet[(this.alphabet.indexOf(char) + this.alphabet.indexOf(keyChar)) % 26];
+        return encrypted;
+      }
+      return char; 
+    });
+
+    if (!this.isDirect) {
+      let reverse = '';
+      for (let i = result.length - 1; i >= 0; i -= 1){
+        reverse += result[i];
+      }
+      return reverse;
+    }
+    return result.join('');
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    let messageArray = message.toUpperCase().split('');
+    let keyArray = key.toUpperCase().split('');
+    let index = 0;
+    let result = messageArray.map((char) => {
+      if (this.alphabet.includes(char)){
+        if (index > keyArray.length - 1) {index = 0}
+        console.log(index, char, keyArray[index]);
+        let keyChar = keyArray[index];
+        index += 1;
+        let encrypted = this.alphabet[(this.alphabet.indexOf(char) - this.alphabet.indexOf(keyChar) + 26) % 26];
+        return encrypted;
+      }
+      return char; 
+    });
+    if (!this.isDirect) {
+      let reverse = '';
+      for (let i = result.length - 1; i >= 0; i -= 1){
+        reverse += result[i];
+      }
+      return reverse;
+    }
+    return result.join('');
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
